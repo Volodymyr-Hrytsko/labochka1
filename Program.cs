aae4b215c8eb46c8f7fe59e006b9c303e90442f1
Volodymyr-Hrytsko <grickovova18@gmail.com>
2020-05-28 21:28:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Laba1
+{
+    public class Program
+    {
+        public static int Suma(string s)
+        {
+
+            char[] c = s.ToCharArray();
+            int[] n = new int[s.Length];
+            int sum = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                n[i] = (int)Char.GetNumericValue(c[i]);
+                sum += n[i];
+
+
+            }
+            Console.WriteLine("Suma: {0}", sum);
+
+            return sum;
+
+        }
+        public static int Dobutok(string s)
+        {
+            char[] c = s.ToCharArray();
+            int[] n = new int[s.Length];
+
+            int dob = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                n[i] = (int)Char.GetNumericValue(c[i]);
+
+                dob *= n[i];
+
+            }
+
+            Console.WriteLine("Dobytok: {0}", dob);
+            return dob;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a four-digit number: ");
+            string s = Console.ReadLine();
+            Suma(s);
+            Dobutok(s);
+        }
+    }
+}
